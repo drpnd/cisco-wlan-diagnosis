17887310efd7a373750f1cbb389d258a7217da7d
Hirochika Asai <panda@jar.jp>
2023-07-19 11:08:11 -0700

    Add OIDs and remove text-based logging.

--- monitor/trap_handler.py
+++ monitor/trap_handler.py
@@ -10,42 +10,47 @@ import fcntl
 import mydb
 import config
 
+## CISCO-LWAPP-DOT11-CLIENT-MIB
+OID_TRAP = '.1.3.6.1.6.3.1.1.4.1.0'
+OID_ASSOC = '.1.3.6.1.4.1.9.9.599.0.9' # ciscoLwappDot11ClientAssocTrap
+OID_ASSOC_STATS = '.1.3.6.1.4.1.9.9.599.0.7' # ciscoLwappDot11ClientAssocDataStatsTrap
+OID_DEASSOC_STATS = '.1.3.6.1.4.1.9.9.599.0.6' # ciscoLwappDot11ClientDisassocDataStatsTrap
+OID_DEAUTH = '.1.3.6.1.4.1.9.9.599.0.10' # ciscoLwappDot11ClientDeAuthenticatedTrap
+OID_APJOIN = '.1.3.6.1.4.1.9.9.513.0.4' # ciscoLwappApAssociated
+
+## AIRESPACE-WIRELESS-MIB
+OID_APREMOVE = '.1.3.6.1.4.1.14179.2.6.3.8' # bsnAPDisassociated
+OID_DFS_DETECTED = '.1.3.6.1.4.1.14179.2.6.3.81' # bsnRadarChannelDetected
+OID_DFS_CLEARED = '.1.3.6.1.4.1.14179.2.6.3.82' # bsnRadarChannelCleared
+
 """
 Main routine
 """
 def main():
-    fp = open(config.TRAP_LOG_FILE, "a+")
-    fcntl.flock(fp.fileno(), fcntl.LOCK_EX)
-
     ts = int(time.time())
     try:
         ## Parse the input
         host = sys.stdin.readline().rstrip()
         connection = sys.stdin.readline().rstrip()
 
-        fp.write("{}\n{}\n{}\n".format(time.time(), host, connection))
-
         rawtext = ''
         data = {}
         for l in sys.stdin:
+            ## Raw text to the database
             rawtext += l
-            fp.write(l)
+            ## Parse to a Key-Value dictionary
             l = l.strip()
             k, v = l.split(" ", 1)
             if v.startswith("Wrong Type"):
                 wmsg, nv = v.split(": ", 1)
                 v = nv
             data[k] = v
-        fp.write("---------------------------------------\n")
     except:
         pass
-    fcntl.flock(fp.fileno(), fcntl.LOCK_UN)
-    fp.close()
 
-    ## Insert into the database
+    ## Insert an entry into the database
     db = mydb.connect()
     c = db.cursor()
-
     sql = '''insert into snmp_trap_log (ts, host, connection, rawtext) values (%s, %s, %s, %s)'''
     c.execute(sql, (ts, host, connection, rawtext))
     db.commit()
